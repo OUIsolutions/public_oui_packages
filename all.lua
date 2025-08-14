b02a92acf1366ffc7d26a5c481554a44ba84849d
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-14 18:53:06 -0300

    att

--- all.lua
+++ all.lua
@@ -1,5 +1,11 @@
 
 
 function update()
-    
+
+    PushBlind.add_package({
+        package_name = "OUIsolutions/public_oui_packages",
+        filename = "vibescript.lua",
+        name = "vibescript",
+        force=false
+    })
 end 
