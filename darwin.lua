06e6bd5fcda3a51e2b1dc1bc7166f101f47f55a0
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-24 03:50:52 -0300

    att

---
+++ darwin.lua
@@ -0,0 +1,27 @@
+
+function PushBlind.actions.install()
+
+    dtw.remove_any("vibescript.out")
+    local version = argv.get_flag_arg_by_index({ "version" },1)
+    if  version then
+        print("version"..version)
+        os.execute('curl -L https://github.com/OUIsolutions/VibeScript/releases/download/' .. version .. '/linux_bin.out -o darwin.out')
+
+    end 
+    if not version then
+        os.execute('curl -L https://github.com/OUIsolutions/VibeScript/releases/latest/download/linux_bin.out -o darwin.out') 
+    end
+    if not dtw.isfile("darwin.out") then
+        print("Error: darwin.out not found")
+        return false
+    end
+
+    os.execute("sudo chmod +x darwin.out ")
+    os.execute("sudo mv darwin.out  /usr/local/bin/darwin")
+
+    return true
+end
+
+function PushBlind.actions.remove()
+    os.execute("sudo rm /usr/local/bin/darwin")
+end
