ebbc7365d2614d5753c3b07b25a0cd6cf00fbba4
Mateus Moutinho Queiroz <mateusmoutinho01@gmail.com>
2025-08-23 23:00:15 -0300

    Update all.lua

--- all.lua
+++ all.lua
@@ -1,7 +1,7 @@
 
 
 function PushBlind.actions.update()
-
+    print("runned")
     PushBlind.add_package({
         package_name = "OUIsolutions/public_oui_packages",
         filename = "vibescript.lua",
