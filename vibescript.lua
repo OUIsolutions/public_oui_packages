d774a0f78ae6ca81314e05ce3f17a6383a9b8d75
Mateus Moutinho Queiroz <mateusmoutinho01@gmail.com>
2025-09-14 22:46:21 -0300

    att

--- vibescript.lua
+++ vibescript.lua
@@ -5,20 +5,19 @@ function PushBlind.actions.install()
     local version = argv.get_flag_arg_by_index({ "version" },1)
     if  version then
         print("version"..version)
-        os.execute('curl -L https://github.com/OUIsolutions/VibeScript/releases/download/' .. version .. '/vibescript.out -o vibescript.out')
+        os.execute('curl -L https://github.com/OUIsolutions/VibeScript/releases/download/' .. version .. '/amalgamation.c -o amalgamation.c')
 
     end 
     if not version then
-        os.execute('curl -L https://github.com/OUIsolutions/VibeScript/releases/latest/download/vibescript.out -o vibescript.out') 
+        os.execute('curl -L https://github.com/OUIsolutions/VibeScript/releases/latest/download/amalgamation.c -o amalgamation.c') 
     end
-    if not dtw.isfile("vibescript.out") then
-        print("Error: vibescript.out not found")
+    if not dtw.isfile("amalgamation.c") then
+        print("Error: amalgamation.c not found")
         return false
     end
-
-    os.execute("sudo chmod +x vibescript.out ")
-    os.execute("sudo mv vibescript.out  /usr/local/bin/vibescript")
-
+    os.execute("gcc  -o vibescript.out amalgamation.c ")
+    dtw.move_any_overwriting("vibescript.out", "/usr/local/bin/vibescript")
+    dtw.remove_any("amalgamation.c")
     return true
 end
 
