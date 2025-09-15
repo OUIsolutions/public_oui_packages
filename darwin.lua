3b079e6c047d2c4155535857bcf13dd2c033f4d5
Mateus Moutinho Queiroz <mateusmoutinho01@gmail.com>
2025-09-14 22:49:04 -0300

    ATTT

--- darwin.lua
+++ darwin.lua
@@ -1,24 +1,23 @@
 
 function PushBlind.actions.install()
 
-    dtw.remove_any("vibescript.out")
+    dtw.remove_any("darwin.out")
     local version = argv.get_flag_arg_by_index({ "version" },1)
     if  version then
         print("version"..version)
-        os.execute('curl -L https://github.com/OUIsolutions/Darwin/releases/download/' .. version .. '/darwin_linux_bin.out -o darwin.out')
+        os.execute('curl -L https://github.com/OUIsolutions/Darwin/releases/download/' .. version .. '/darwin.c -o darwin.c')
 
     end 
     if not version then
-        os.execute('curl -L https://github.com/OUIsolutions/Darwin/releases/latest/download/darwin_linux_bin.out -o darwin.out') 
+        os.execute('curl -L https://github.com/OUIsolutions/Darwin/releases/latest/download/darwin.c -o darwin.c') 
     end
-    if not dtw.isfile("darwin.out") then
-        print("Error: darwin.out not found")
+    if not dtw.isfile("darwin.c") then
+        print("Error: darwin.c not found")
         return false
     end
-
-    os.execute("sudo chmod +x darwin.out ")
-    os.execute("sudo mv darwin.out  /usr/local/bin/darwin")
-
+    os.execute("gcc  -o darwin.out darwin.c ")
+    dtw.move_any_overwriting("darwin.out", "/usr/local/bin/darwin")
+    dtw.remove_any("darwin.c")
     return true
 end
 
