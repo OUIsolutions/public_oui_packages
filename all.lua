c4b359cedffae6297e1ef8623dfb11d85f74013e
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-10-07 19:30:07 -0300

    att

--- all.lua
+++ all.lua
@@ -1,4 +1,23 @@
+function PushBlind.actions.set_repo()
+   local repo_name = argv.get_next_unused()
+   if not repo_name then
+      error("public_oui repo not found usage: pushblind set_repo public_oui <cachify_repo>")
+   end
+   local path = dtw.get_absolute_path(repo_name)
+   if not path then
+      error("This repo does not exist")
+   end
+   set_prop("public_oui_repo",path)
+end
 
+
+function PushBlind.actions.code_open()
+    local repo = get_prop("public_oui_repo")
+    if not repo then
+        error("You need to run: 'pushblind set_repo public_oui <public_oui_repo>' first")
+    end
+    os.execute("code "..repo)
+end
 
 function PushBlind.actions.update()
         PushBlind.add_package({
