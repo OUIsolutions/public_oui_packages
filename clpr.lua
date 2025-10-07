abf2d6c2e735b60053c65afcf08bb7a797d24566
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-10-07 19:49:58 -0300

    att

--- clpr.lua
+++ clpr.lua
@@ -1,19 +1,19 @@
 function PushBlind.actions.set_repo()
    local repo_name = argv.get_next_unused()
    if not repo_name then
-      error("public_oui repo not found usage: pushblind set_repo public_oui <cachify_repo>")
+      error("public_oui repo not found usage: pushblind set_repo clpr <cachify_repo>")
    end
    local path = dtw.get_absolute_path(repo_name)
    if not path then
       error("This repo does not exist")
    end
-   set_prop("public_oui_repo",path)
+   set_prop("clpr",path)
 end
 
 function PushBlind.actions.publish()
-    local repo = get_prop("cachify_repo")
+    local repo = get_prop("clpr")
     if not repo then
-        error("You need to run: 'pushblind set_repo cachify <cachify_repo>' first")
+        error("You need to run: 'pushblind set_repo clpr <clpr_repo>' first")
     end
     os.execute("cd "..repo.." && darwin run_blueprint --target all")
     os.execute("cd "..repo.." && vibescript shipyard  release.json")
@@ -23,9 +23,9 @@ end
 
 
 function PushBlind.actions.code()
-    local repo = get_prop("public_oui_repo")
+    local repo = get_prop("clpr")
     if not repo then
-        error("You need to run: 'pushblind set_repo public_oui <public_oui_repo>' first")
+        error("You need to run: 'pushblind set_repo clpr <clpr_repo>' first")
     end
     os.execute("cd "..repo.." && git pull")
     os.execute("code "..repo)
