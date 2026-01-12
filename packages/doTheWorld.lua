0e66f3d68b1d8efd88934de53831f22a1f192be4
mateusmoutinho <mateusmoutinho01@gmail.com>
2026-01-12 15:31:06 -0300

    att

--- packages/doTheWorld.lua
+++ packages/doTheWorld.lua
@@ -2,3 +2,12 @@ relative_load('../utils/actions_factory.lua')
 create_default_actions("doTheWorld")
 
 
+function PushBlind.actions.build()
+    local repo = get_prop("doTheWorld_repo")
+    if not repo then
+        error("You need to run: 'pushblind set_repo doTheWorld <doTheWorld_repo>' first")
+    end
+
+    os.execute("cd " .. repo .. " && darwin install darwindeps.json --soft")
+    os.execute("cd " .. repo .. " && darwin run_blueprint --target all")
+end
