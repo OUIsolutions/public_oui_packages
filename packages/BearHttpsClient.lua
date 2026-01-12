73bff8524d3be70e8054575eef2b1dc39d05b3e6
mateusmoutinho <mateusmoutinho01@gmail.com>
2026-01-12 17:21:17 -0300

    att

--- packages/BearHttpsClient.lua
+++ packages/BearHttpsClient.lua
@@ -2,11 +2,15 @@ relative_load('../utils/actions_factory.lua')
 create_default_actions("BearHttpsClient")
 
 function PushBlind.actions.build_deps()
-        local repo = get_prop("BearHttpsClient_repo")
-    if not repo then
+    local bear_repo = get_prop("BearHttpsClient_repo")
+    if not bear_repo then
         error("You need to run: 'pushblind set_repo BearHttpsClient <BearHttpsClient_repo>' first")
     end
-    os.execute("cd " .. repo .. " && darwin install darwindeps.json --soft")
+
+    PushBlind.run_action("c2wasm","build")
+    move_dep("c2wasm", "c2wasm.c", "BearHttpsClient", "dependencies/c2wasm.c")
+
+    os.execute("cd " .. bear_repo .. " && darwin install darwindeps.json --soft")
 end
 
 function PushBlind.actions.build()
