3f01ad37b30d0054d098445d5f2c84503959b2ba
mateusmoutinho <mateusmoutinho01@gmail.com>
2026-01-13 04:54:24 -0300

    att

---
+++ packages/luaargv.lua
@@ -0,0 +1,13 @@
+relative_load('../utils/actions_factory.lua')
+relative_load('../utils/utils.lua')
+
+create_default_actions("luaargv")
+
+function PushBlind.actions.build()
+    local repo = get_prop("BearHttpsClient_repo")
+    if not repo then
+        error("You need to run: 'pushblind set_repo BearHttpsClient <BearHttpsClient_repo>' first")
+    end
+    
+    os.execute("cd " .. repo .. " && darwin run_blueprint --target all")
+end
