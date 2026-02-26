802222d27fb9c81a5f912918e73dd6c6af76ce85
mateusmoutinho <mateusmoutinho01@gmail.com>
2026-02-26 03:09:40 -0300

    att

---
+++ utils/utils.lua
@@ -0,0 +1,25 @@
+
+function build_deps(props)
+    local project_repo = get_prop(props.project.."_repo")
+    if not project_repo then
+        error("You need to run: 'pushblind set_repo " .. props.project .. " <" .. props.project .. "_repo>' first")
+    end
+    local dep_repo = get_prop(props.dep.."_repo")
+    if not dep_repo then
+        error("You need to run: 'pushblind set_repo " .. props.dep .. " <" .. props.dep .. "_repo>' first")
+    end
+
+    for _, action in ipairs(props.actions) do
+        PushBlind.run_action(props.dep, action)
+    end
+
+    for _, source in ipairs(props.sources) do
+        local src_path = dep_repo .. "/" .. source.target
+        if not dtw.isfile(src_path) then
+            error("File not found: " .. src_path)
+        end
+        local dest_path = project_repo .. "/" .. source.dest
+        dtw.copy_any_overwriting(src_path, dest_path)
+    end
+
+end
