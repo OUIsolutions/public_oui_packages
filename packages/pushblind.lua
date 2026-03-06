bff7b03121dfb6daccc52abd275612738171c8dd
kauaHenSilva <kaua.sbc@gmail.com>
2026-03-05 21:04:34 -0300

    feat: add install to pushblind

---
+++ packages/pushblind.lua
@@ -0,0 +1,33 @@
+relative_load('../utils/actions_factory.lua')
+relative_load('../utils/utils.lua')
+create_default_actions("pushblind")
+
+function PushBlind.actions.repo_install()
+    local repo = get_prop("pushblind_repo")
+    if not repo then
+        error("You need to run: 'pushblind set_repo pushblind <pushblind_repo>' first")
+    end
+
+    os.execute("cd " .. repo .. " && darwin install --soft")
+    os.execute("cd " .. repo .. " && darwin run_blueprint --target all")
+    
+    if os_name == "linux" then
+        os.execute("sudo dpkg -i " .. repo .. "/release/pushblind.deb")
+    end
+end
+
+-- function PushBlind.actions.publish()
+--     local repo = get_prop("pushblind_repo")
+--     if not repo then
+--         error("You need to run: 'pushblind set_repo pushblind <pushblind_repo>' first")
+--     end
+
+--     dtw.remove_any(repo .. "/dependencies")
+--     dtw.remove_any(repo .. "/release")
+
+--     os.execute("cd " .. repo .. " && darwin install darwindeps.json")
+--     os.execute("cd " .. repo .. " && darwin run_blueprint --target all")
+--     os.execute("cd " .. repo .. " && vibescript shipyard release.json")
+--     print("✅ Published VibeScript repo at: " .. repo)
+-- end
+
