b2e45313b975c4e128792414f0c208832563f561
augustofrade <aquaaticnaotrava@gmail.com>
2026-03-11 20:05:38 -0300

    Add luacembed build_deps action

--- packages/luacembed.lua
+++ packages/luacembed.lua
@@ -3,6 +3,28 @@
 relative_load('../utils/actions_factory.lua')
 create_default_actions("luacembed")
 
+function PushBlind.actions.build_deps()
+    build_deps({
+        project = "luacembed",
+        dep = "UniversalGarbageCollector",
+        actions = {"build"},
+        sources = {
+            { target = "release/UniversalGarbage.h", dest = "dependencies/UniversalGarbage.h" },
+            { target = "release/UniversalGarbage.c", dest = "dependencies/UniversalGarbage.c" },
+        }
+    })
+
+    build_deps({
+        project = "luacembed",
+        dep = "luaSingleUnity",
+        actions = {"build"},
+        sources = {
+            { target = "release/lua_single_unity_classic_onelua.c", dest = "dependencies/lua_single_unity_classic_onelua.c" },
+            { target = "release/lua_single_unity.h", dest = "dependencies/lua_single_unity.h" },
+        }
+    })
+end
+
 function PushBlind.actions.publish()
    local repo = get_prop("luacembed_repo")
     if not repo then
