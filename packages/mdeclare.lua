fd221f8f616770c48984524e1292b8158971e3e9
augustofrade <aquaaticnaotrava@gmail.com>
2026-03-17 22:02:17 -0300

    Add mdeclare binary installation

--- packages/mdeclare.lua
+++ packages/mdeclare.lua
@@ -9,3 +9,14 @@ function PushBlind.actions.build()
 
   os.execute("cd " .. repo .. " && darwin run_blueprint build/ --mode folder amalgamation_build")
 end
+
+function PushBlind.actions.install()
+  PushBlind.run_action("mdeclare", "build")
+  if not repo then
+    error("You need to run: 'pushblind set_repo mdeclare <mdeclare_repo>' first")
+  end
+
+  os.execute("cd " .. repo .. "/release && gcc MDeclare.c -o mdeclare")
+  os.execute("mv " .. repo .. "/release/mdeclare ~/.local/bin/mdeclare")
+  print("MDeclare installed successfully as 'mdeclare' command.")
+end
