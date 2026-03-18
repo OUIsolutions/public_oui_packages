41300521069fce81fd633217b88887ff94aaa2ba
augustofrade <aquaaticnaotrava@gmail.com>
2026-03-17 23:08:41 -0300

    Add luaShip package

---
+++ packages/luaShip.lua
@@ -0,0 +1,11 @@
+relative_load('../utils/actions_factory.lua')
+create_default_actions("luaShip")
+
+function PushBlind.actions.build()
+  local repo = get_prop("luaShip_repo")
+  if not repo then
+    error("You need to run: 'pushblind set_repo luaShip <luaShip_repo>' first")
+  end
+
+  os.execute("cd " .. repo .. " && darwin run_blueprint")
+end
