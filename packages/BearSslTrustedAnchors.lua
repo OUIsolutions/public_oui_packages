f1f50b1d90306510aa52f14c9842c88f54875ce2
augustofrade <aquaaticnaotrava@gmail.com>
2026-03-27 22:44:28 -0300

    Add BearSslTrustedAnchors package

---
+++ packages/BearSslTrustedAnchors.lua
@@ -0,0 +1,13 @@
+relative_load('../utils/actions_factory.lua')
+relative_load('../utils/utils.lua')
+
+create_default_actions("BearSslTrustedAnchors")
+
+function PushBlind.actions.build()
+  local repo = get_prop("BearSslTrustedAnchors_repo")
+  if not repo then
+    error("You need to run: 'pushblind set_repo BearSslTrustedAnchors <BearSslTrustedAnchors_repo>' first")
+  end
+
+  os.execute("cd " .. repo .. " && chmod +x ./generator.sh && ./generator.sh")
+end
