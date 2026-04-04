c0afdf091af70cedd7b200cc63571b971df24d79
Mateus Moutinho Queiroz <mateusmoutinho01@gmail.com>
2026-04-04 18:36:41 -0300

    att

--- packages/pushblind.lua
+++ packages/pushblind.lua
@@ -3,14 +3,23 @@ relative_load('../utils/utils.lua')
 create_default_actions("pushblind", "OUIsolutions/PushBlind")
 function PushBlind.actions.scratch_install()
     PushBlind.run_action("vibescript","build_deps") 
-    local repo = get_prop("vibescript_repo")
-    if not repo then
+    local vibescript_repo = get_prop("vibescript_repo")
+    if not vibescript_repo then
         error("You need to run: 'pushblind set_repo vibescript <vibescript_repo>' first")
     end
 
-    os.execute("cd " .. repo .. " && darwin run_blueprint --target amalgamation")
+    os.execute("cd " .. vibescript_repo .. " && darwin run_blueprint --target amalgamation")
 
-    
+    local pushblind_repo = get_prop("pushblind_repo")
+    if not pushblind_repo then
+        error("You need to run: 'pushblind set_repo pushblind <pushblind_repo>' first")
+    end
+    if not darwin.dtw.isfile(vibescript_repo .. "/release/amalgamation.c") then
+        error("impossible to generate vibescript amalgamation.c")
+    end
+    darwin.dtw.copy_any_overwriting(vibescript_repo .. "/release/amalgamation.c", pushblind_repo .. "/dependencies/vibescript.c")
+
+
 end
 function PushBlind.actions.repo_install()
     local repo = get_prop("pushblind_repo")
