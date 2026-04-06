981fa349d85b8f1444e4add00ba2b1092fe58332
Mateus Moutinho Queiroz <mateusmoutinho01@gmail.com>
2026-04-06 15:15:00 -0300

    att

--- packages/BearHttpsClient.lua
+++ packages/BearHttpsClient.lua
@@ -71,3 +71,19 @@ function PushBlind.actions.build()
     end
     os.execute("cd " .. repo .. " && darwin run_blueprint --target all")
 end
+
+function PushBlind.actions.publish()
+    local repo = get_prop("BearHttpsClient_repo")
+    if not repo then
+        error("You need to run: 'pushblind set_repo BearHttpsClient <BearHttpsClient_repo>' first")
+    end
+
+    dtw.remove_any(repo.."/dependencies")
+    dtw.remove_any(repo.."/release")
+
+    os.execute("cd "..repo.." && darwin install darwindeps.json ")
+    os.execute("cd "..repo.." && darwin run_blueprint --target all")
+    os.execute("cd "..repo.." && vibescript shipyard  release.json")
+    print("Published to repo "..repo)
+
+end
